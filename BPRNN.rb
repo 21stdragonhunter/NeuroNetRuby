04cd547ecfaf42f5f12c7a9de30bc39855f85698
21stdragonhunter <colemansmith21@gmail.com>
2015-01-20 09:20:20 -0500

    Create BPRNN.rb

---
+++ BPRNN.rb
@@ -0,0 +1,7 @@
+$AUTHOR = 'Coleman'
+
+# implement recurrent neural network with sigmoid, hyperbolic, step, sign, threshold, and squash function
+# mark a weight as constant
+# cross entropy revision error propagation, and cross entropy derivative error propagation
+# cross entropy - mean sum error
+# derivative - mean sum squared error
