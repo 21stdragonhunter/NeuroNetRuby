1941e8a683341b7c8f3c7d1dd5ff13ec2f157f82
21stdragonhunter <colemansmith21@gmail.com>
2015-01-20 09:27:01 -0500

    Equations listed

--- BPRNN.rb
+++ BPRNN.rb
@@ -5,3 +5,12 @@ $AUTHOR = 'Coleman'
 # cross entropy revision error propagation, and cross entropy derivative error propagation
 # cross entropy - mean sum error
 # derivative - mean sum squared error
+# MSE cost-
+#     weight update --- input * output_delta
+#     neuron error --- sum(output_delta * output_weight) * sigma_prime
+#     output error --- sigma_prime * (target - actual)
+#
+# CE cost-
+#     weight update --- input * output_delta
+#     neuron error --- sum(output_delta * output_weight) / num_output
+#     output error --- target - actual
